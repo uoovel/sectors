da34972a128943fe0c9c1e0365ad53c0afda7bd2
Urmas Öövel <uoovel@itcollege.ee>
2023-07-14 16:13:27 +0300

    Project inited

---
+++ src/main/java/com/codeurmas/sectors/SectorsApplication.java
@@ -0,0 +1,13 @@
+package com.codeurmas.sectors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SectorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SectorsApplication.class, args);
+	}
+
+}
